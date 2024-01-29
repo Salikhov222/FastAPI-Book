9dee588248bb0b14a596947b6542363456d17b58
akhmed <salikhov.off@gmail.com>
2024-01-29 13:09:29 +0300

    Initialization of MongoDB and implementation of CRUD methods

--- planner/models/users.py
+++ planner/models/users.py
@@ -1,11 +1,16 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+
+from pydantic import BaseModel, EmailStr
+from beanie import Document, Link
 from models.events import Event
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
-    events: Optional[List[Event]]
+    events: Optional[List[Link[Event]]]
+
+    class Settings:
+        name = "users"
 
     class Config:
         json_schema_extra = {
