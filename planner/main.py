6e36e526588ae088a4f342b13633a22130ce9ca6
akhmed <salikhov.off@gmail.com>
2024-01-29 11:44:47 +0300

    [Feature] Incorporate a SQL database and implement CRUD operations

--- planner/main.py
+++ planner/main.py
@@ -1,13 +1,27 @@
 import uvicorn
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from database.connection import conn
 from routes.users import user_router
 from routes.events import event_router
 
+# Функция с асинхронным менеджером контекста, которая выполняется перед тем, как приложение начнет получать запросы
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
--- planner/models/events.py
+++ planner/models/events.py
@@ -1,22 +1,41 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from sqlmodel import JSON, SQLModel, Field, Column
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
+
+    class Config:
+        arbitray_types_allowed = True
+        json_schema_extra = {
+            "example": {
+            "title": "FastAPI Book Launch",
+            "image": "https:linktomyimage.com/image.png",
+            "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+            "tags": ["python", "fastapi", "book", "launch"],
+            "location": "Google Meet"
+            }
+        }
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         json_schema_extra = {
             "example": {
-                "id": 1,
-                "title": "FastAPI Book Launch",
-                "image": "https:linktomyimage.com/image.png",
-                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
-                "tags": ["python", "fastapi", "book", "launch"],
-                "location": "Google Meet"
+            "title": "FastAPI Book Launch",
+            "image": "https:linktomyimage.com/image.png",
+            "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+            "tags": ["python", "fastapi", "book", "launch"],
+            "location": "Google Meet"
             }
         }
